2bc796a3b52041d93dfb7fa34d690b840157f714
Aditya <adimail2404@gmail.com>
2024-07-30 17:23:59 +0530

    remove leading number text

--- lib/src/level_selection/level_selection_screen.dart
+++ lib/src/level_selection/level_selection_screen.dart
@@ -1,7 +1,3 @@
-// Copyright 2022, the Flutter project authors. Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
@@ -51,7 +47,6 @@ class LevelSelectionScreen extends StatelessWidget {
                         GoRouter.of(context)
                             .go('/play/session/${level.number}');
                       },
-                      leading: Text(level.number.toString()),
                       title: Text('Team ${level.number}'),
                     )
                 ],
