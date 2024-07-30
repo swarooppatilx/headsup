fc02ea94dbce5a9b6bc095b2b1e671212e32c04b
Aditya <adimail2404@gmail.com>
2024-07-30 20:58:46 +0530

    Added words object constructor in teams obj

--- lib/src/level_selection/teams.dart
+++ lib/src/level_selection/teams.dart
@@ -4,26 +4,29 @@
 
 const teams = [
   Team(
-    number: 1,
-    difficulty: 5,
-    // TODO: When ready, change these achievement IDs.
-    // You configure this in App Store Connect.
-    achievementIdIOS: 'first_win',
-    // You get this string when you configure an achievement in Play Console.
-    achievementIdAndroid: 'NhkIwB69ejkMAOOLDb',
-  ),
+      number: 1,
+      difficulty: 5,
+      // TODO: When ready, change these achievement IDs.
+      // You configure this in App Store Connect.
+      achievementIdIOS: 'first_win',
+      // You get this string when you configure an achievement in Play Console.
+      achievementIdAndroid: 'NhkIwB69ejkMAOOLDb',
+      words: ['example', 'words', 'for', 'team', '1']),
   Team(
-    number: 2,
-    difficulty: 100,
-    achievementIdIOS: 'finished',
-    achievementIdAndroid: 'CdfIhE96aspNWLGSQg',
-  ),
+      number: 2,
+      difficulty: 100,
+      achievementIdIOS: 'finished',
+      achievementIdAndroid: 'CdfIhE96aspNWLGSQg',
+      words: ['example', 'words', 'for', 'team', '2']),
 ];
 
 class Team {
   final int number;
 
   final int difficulty;
+
+  /// The list of words associated with the team.
+  final List<String> words;
 
   /// The achievement to unlock when the level is finished, if any.
   final String? achievementIdIOS;
@@ -35,6 +38,7 @@ class Team {
   const Team({
     required this.number,
     required this.difficulty,
+    required this.words,
     this.achievementIdIOS,
     this.achievementIdAndroid,
   }) : assert(
